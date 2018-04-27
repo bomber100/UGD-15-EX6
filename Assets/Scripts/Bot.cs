a8fc1a5a9b809d0778f66c3bed7eb7e72961a227
Alexej Tolkachov <aleksej.tolkachov@gmail.com>
2018-04-27 23:10:38 +0300

    bot and game over scene added

---
+++ Assets/Scripts/Bot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Bot : MonoBehaviour {
+
+    public Transform pointA;
+    public Transform pointB;
+    NavMeshAgent agent;
+
+    void Start() {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = pointA.position;
+    }
+
+    void Update() {
+        if (agent.transform.position.x == pointA.position.x && agent.transform.position.z == pointA.position.z) {
+            agent.SetDestination(pointB.position);
+            print("A");
+        }
+        if (agent.transform.position.x == pointB.position.x && agent.transform.position.z == pointB.position.z) {
+            agent.SetDestination(pointA.position);
+            print("b");
+        }
+    }
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour {
+    public GameObject gameOverPanel;
+
+    void Start() {
+        gameOverPanel.SetActive(false);
+    }
+    void Update() {
+        if (PlayerController.health <= 0) {
+            gameOverPanel.SetActive(true);
+
+        }
+    }
+    public void Button() {
+        gameOverPanel.SetActive(false);
+        PlayerController.health = 100;
+        SceneManager.LoadScene("Labyrinth", LoadSceneMode.Single);
+    }
+}
